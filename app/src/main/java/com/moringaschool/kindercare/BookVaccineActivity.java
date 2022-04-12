c6d1e1905fb0beb6989d9af7b6b83734ed590d81
Louis-crypto <kinglouis745@gmail.com>
2022-04-13 00:53:54 +0300

    create a time-picker and date-picker for appointment

--- app/src/main/java/com/moringaschool/kindercare/BookVaccineActivity.java
+++ app/src/main/java/com/moringaschool/kindercare/BookVaccineActivity.java
@@ -2,13 +2,84 @@ package com.moringaschool.kindercare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class BookVaccineActivity extends AppCompatActivity {
+
+    EditText appointmentPicker;
+    EditText timePicker;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_vaccine);
+
+        appointmentPicker = findViewById(R.id.appointmentDatePicker);
+        timePicker = findViewById(R.id.timePicker);
+
+        timePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog = new TimePickerDialog(BookVaccineActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        timePicker.setText(hourOfDay + ":" + minute);
+                    }
+                }, 0, 0, false);
+                timePickerDialog.show();
+            }
+        });
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        appointmentPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(BookVaccineActivity.this, android.R.style.Widget_Holo_Light_ActionBar
+                , setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1 ;
+                String date = day+"/"+month+"/"+year;
+                appointmentPicker.setText(date);
+            }
+        };
+
+        appointmentPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        BookVaccineActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day+"/"+month+"/"+year;
+                        appointmentPicker.setText(date);
+                    }
+                } ,year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 }
